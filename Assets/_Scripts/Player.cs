5f9e2be73cedcf1de9e9e5861df4dce5971a74f1
Alam Rodriguez <alamgrodriguez@gmail.com>
2022-08-31 16:15:27 -0500

    Player update

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -9,11 +9,15 @@ namespace CharacterController2D
         [Header("Movement attributes")]
         [Tooltip("Movement speed")]
         [SerializeField] private float movementSpeed;
-        [Range(0.1f, 20f), Tooltip("The amount of speed reduction per second when the player is not moving")]
+        [Tooltip("When this is checked, the player will stop immediately if it is not receiving a movement input.")]
+        [SerializeField] private bool stopMovementImmediately;
+        [Range(0.1f, 30f), Tooltip("The amount of speed reduction per second when the player is not moving")]
         [SerializeField] private float decelerationStrength;
         [Header("Jump attributes")]
         [Tooltip("Jump force")]
         [SerializeField] private float jumpForce;
+        [SerializeField] AnimationCurve jumpCurve;
+        [SerializeField] float jumpCurveTotalTime;
         [Tooltip("Player's gravity scale")]
         [SerializeField] private float gravityScale;
         [Header("Other attributes")]
@@ -23,12 +27,17 @@ namespace CharacterController2D
         private Rigidbody2D rigidB;
         private Collider2D playerCollider;
         private Vector2 movementDirection;
+        private float jumpCurveDelta;
+        private bool isJumping = false;
         #endregion
 
         void Start()
         {
+            #region GET COMPONENTS
             rigidB = GetComponent<Rigidbody2D>();
             playerCollider = GetComponent<Collider2D>();
+            #endregion
+
         }
 
         void Update()
@@ -37,7 +46,6 @@ namespace CharacterController2D
             if (rigidB.gravityScale != gravityScale)
                 rigidB.gravityScale = gravityScale;
 
-            movementDirection.y = 0f;
             float HorizontalInput = Input.GetAxisRaw("Horizontal");
 
             if(HorizontalInput != 0f)
@@ -47,35 +55,68 @@ namespace CharacterController2D
             }
             else
             {
-                //Decrease speed over time
-                if (Mathf.Abs(movementDirection.x) >= 0.1f)
-                    movementDirection.x += -Mathf.Sign(movementDirection.x) * decelerationStrength * Time.deltaTime;
-                else
+                //Stop movement
+                if (stopMovementImmediately || Mathf.Abs(movementDirection.x) <= 0.1f) 
                     movementDirection.x = 0f;
+                else //Reduce movement gradually
+                    movementDirection.x += -Mathf.Sign(movementDirection.x) * decelerationStrength * Time.deltaTime;
             }
 
+            if (IsGrounded())
+            {
+                Debug.Log("Is Grounded");
+                AvoidEdgeSlide();
+                movementDirection.y = 0f;
+                isJumping = false;
+            }
+            else
+            {
+                rigidB.gravityScale = gravityScale;
+                movementDirection.y = rigidB.velocity.y;
+            }
 
             #region JUMP
-            Debug.DrawRay(playerCollider.bounds.center, Vector2.down * (playerCollider.bounds.extents.y + 0.05f), Color.green);
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
             {
                 //Check if the player is on the ground
-                if (Physics2D.BoxCast(playerCollider.bounds.center, new Vector2(playerCollider.bounds.size.x - 0.1f, 0.05f), 0f, Vector2.down, playerCollider.bounds.extents.y, maskIgnoreSelf))
+                jumpCurveDelta = 0f;
+                isJumping = true;
+                //rigidB.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
+            else if(Input.GetKey(KeyCode.Space) && !isJumping)
+            {
+                if(jumpCurveDelta <= 1f)
                 {
-                    //print("Jump");
-                    rigidB.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                    movementDirection.y = jumpCurve.Evaluate(jumpCurveDelta) * jumpForce;
+                    //Debug.Log(movementDirection.y);
+                    jumpCurveDelta += Time.deltaTime / jumpCurveTotalTime;
                 }
             }
             #endregion
 
-            movementDirection.y = rigidB.velocity.y;
-
-            
+            //AvoidEdgeSlide();
         }
 
         private void FixedUpdate()
         {
             rigidB.velocity = movementDirection;
         }
-    } 
+
+        private bool IsGrounded()
+        {
+            Debug.DrawRay(playerCollider.bounds.center, Vector2.down * (playerCollider.bounds.extents.y + 0.05f), Color.blue);
+
+            return Physics2D.BoxCast(playerCollider.bounds.center, 
+                new Vector2(playerCollider.bounds.size.x - 0.1f, 0.05f), 0f, 
+                Vector2.down, playerCollider.bounds.extents.y + 0.05f, maskIgnoreSelf);
+        }
+
+        private void AvoidEdgeSlide()
+        {
+            rigidB.velocity = Vector2.zero;
+            rigidB.angularVelocity = 0f;
+            rigidB.gravityScale = 0f;
+        }
+
+    }
 }
